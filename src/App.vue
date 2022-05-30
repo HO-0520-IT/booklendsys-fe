367e80d102228a524ec642bccc30c8d519bb1ec5
ほ～ (HO_0520) <62166171+HO-0520-IT@users.noreply.github.com>
2022-05-30 11:46:26 +0900

    処理中ダイアログ追加・メッセージをダイアログに

--- src/App.vue
+++ src/App.vue
@@ -73,6 +73,82 @@
         </v-btn>
       </template>
     </v-snackbar>
+    <v-dialog
+      v-model="this.$store.state.success"
+      width="500"
+    >
+      <v-card>
+        <v-card-title>
+          <v-icon
+            color="green"
+            class="headline"
+          >
+            mdi-check
+          </v-icon>
+          <span class="headline">
+            処理が完了しました
+          </span>
+        </v-card-title>
+        <v-card-text>
+          {{ successMessage }}
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darker" text @click="close_dialog_success">
+            OK
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog
+      v-model="this.$store.state.error"
+      width="500"
+    >
+      <v-card>
+        <v-card-title>
+          <v-icon
+            color="red"
+            class="headline"
+          >
+            mdi-alert-circle
+          </v-icon>
+          <span class="headline">
+            処理が失敗しました
+          </span>
+        </v-card-title>
+        <v-card-text>
+          {{ errorMessage }}
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="red darker" text @click="close_dialog_error">
+            OK
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog
+      v-model="dialog_wait"
+      persistent
+      width="500"
+    >
+      <v-card>
+        <v-card-title>
+          <span class="headline">
+            処理中です
+          </span>
+        </v-card-title>
+        <div class="text-center">
+          <v-progress-circular
+            indeterminate
+            color="green"
+          ></v-progress-circular>
+          <v-card-text>
+            処理中です。しばらくお待ちください。
+          </v-card-text>
+        </div>
+      </v-card>
+    </v-dialog>
   </v-app>
 </template>
 
@@ -86,6 +162,9 @@ export default {
       drawer: false,
       snackbar: false,
       snackbar_s: false,
+      dialog_success: false,
+      dialog_error: false,
+      dialog_wait: false,
       items: [
         { text: "サーバーに接続", icon: "mdi-database-settings", link: "/connectsettings" },
         { text: "蔵書一覧", icon: "mdi-book", link: "/booklist" },
@@ -94,21 +173,39 @@ export default {
       ]
     }
   },
-  computed: mapState({
-    appName: state => state.settings.appName,
-    errorMessage: state => state.errorMessage,
-    successMessage: state => state.successMessage
-  }),
+  computed: {
+    ...mapState({
+      appName: state => state.settings.appName,
+      wait: state => state.wait,
+      errorMessage: state => state.errorMessage,
+      successMessage: state => state.successMessage,
+      showErrorMessage: state => state.error,
+      showSuccessMessage: state => state.success,
+    }),
+  },
+  watch: {
+    wait (val) {
+      this.dialog_wait = val;
+    },
+    showErrorMessage () {
+      this.dialog_error = true;
+      state=>state.error = false;
+    },
+    showSuccessMessage () {
+      this.dialog_success = true;
+      state=>state.success = false;
+    },
+  },
+  methods :{
+    close_dialog_error () {
+      this.$store.commit("toggleDialog_error")
+    },
+    close_dialog_success () {
+      this.$store.commit("toggleDialog_success")
+    },
+  },
   beforeCreate () {
     this.$store.dispatch('loadSettings')
   },
-  watch: {
-    errorMessage () {
-      this.snackbar = true;
-    },
-    successMessage () {
-      this.snackbar_s = true;
-    }
-  }
 }
 </script>
